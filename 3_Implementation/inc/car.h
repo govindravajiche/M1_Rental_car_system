7cc2a9b1f319f5b9aa088ac7f20bdf9d528fe684
govind <ravajiche06govind@gmail.com>
2022-04-02 20:33:15 +0530

    first commit

--- 3_Implementation/inc/car.h
+++ 3_Implementation/inc/car.h
@@ -1,12 +1,4 @@
 int date();
-struct car
-{
-    char customername[50];
-    char customerId[13];
-    char carrented[20];
-    int n,rent,days;
-    int dr,mr,yr,dret,mret,yret;
-};
 char carbrand[10][20]={"MARUTI SUZUKI","HYUNDAI","HONDA","TOYOTA","FORD","TATA","MERCEDES","SKODA","KIA"};
 char carmodel[10][20]={"SWIFT","NEXO","Civic","Avalon","Explorer","Safari","E-Class","RAPID","SELTOS"};
 int carseat[10]={4,4,4,8,4,4,4,8,8,0};
---
+++ 3_Implementation/src/car2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include"car.h"
+struct car 
+{
+    char customername[50];
+    char customerId[13];
+    char carrented[20];
+    int n,rent,days;
+    int dr,mr,yr,dret,mret,yret;
+};
