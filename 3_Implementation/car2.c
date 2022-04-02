5dd19985cc9039383207e693474a9188f07526e9
govind <ravajiche06govind@gmail.com>
2022-04-02 11:57:27 +0530

    first commit

--- 3_Implementation/car2.c
+++ 3_Implementation/car2.c
@@ -2,6 +2,14 @@
 #include<string.h>
 #include<stdlib.h>
 #include"car.h"
+struct car
+{
+    char customername[50];
+    char customerId[13];
+    char carrented[20];
+    int n,rent,days;
+    int dr,mr,yr,dret,mret,yret;
+};
 void car_rent()
 {
     int i,f;
